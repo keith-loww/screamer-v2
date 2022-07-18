baa66a8caddbb6385de2496a66bfab05b9ac6660
AsianCrouton <keithlow1@hotmail.com>
2022-07-18 11:20:39 -0700

    user model

---
+++ model/User.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    username: {
+        type: String
+    },
+    created_at: {
+        type: Date,
+        required: true
+    },
+    picture: {
+        type: String
+    },
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post"
+        }
+    ]
+
+})
+
+userSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+export default mongoose.models.User || mongoose.model("User", userSchema);
