d738c4212cb4fcdca13c25ed58cf836b2a2dd318
AsianCrouton <keithlow1@hotmail.com>
2022-07-18 13:16:40 -0700

    router by id

---
+++ pages/api/users/[id].ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import User from "../../../model/User";
+
+export default async function handler(req: NextApiRequest, res : NextApiResponse) {
+    const {method} = req;
+    const {id} = req.query
+    
+    switch(method) {
+        case "GET":
+            try {
+                const user = await User.findById(id)
+                return res.status(200).json({
+                    success: true,
+                    data: user
+                })
+            } catch (error) {
+                return res.status(400).json({ success: false })
+            }
+        default:
+            return res.status(400).json({ success: false })
+    }
+}
