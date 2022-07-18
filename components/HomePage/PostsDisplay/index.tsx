9f08b0e9ed6b9bae2d4aed5bf9b4128a6752cf18
AsianCrouton <keithlow1@hotmail.com>
2022-07-18 01:33:00 -0700

    get server side props

--- components/HomePage/PostsDisplay/index.tsx
+++ components/HomePage/PostsDisplay/index.tsx
@@ -1,10 +1,18 @@
+import axios from 'axios';
 import React from 'react'
 import { Post } from '../../types'
 
 export default function PostsDisplay({posts} : {post: Post[]}): JSX.Element {
     return (
-        
+
     )
 }
 
-export default function GetServerSideProps()
+export async function GetServerSideProps() {
+    const posts = await axios.get("/api/posts");
+    return {
+        props: {
+            posts
+        }
+    }
+}
