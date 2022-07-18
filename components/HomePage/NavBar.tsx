e22803b8673c8e6b026bc045ca1bf4aedf795080
AsianCrouton <keithlow1@hotmail.com>
2022-07-17 21:30:00 -0700

    navbar login button

--- components/HomePage/NavBar.tsx
+++ components/HomePage/NavBar.tsx
@@ -4,13 +4,20 @@ import { IoMegaphoneSharp } from 'react-icons/io5'
 
 export default function NavBar() {
     return (
-        <div className='navbar shadow-md'>
-            <Link href="/">
-            <a className="btn btn-ghost text-xl space-x-2">
-                <IoMegaphoneSharp />
-                <span>SCREAMER V2</span>
-            </a>
-            </Link>
+        <div className='navbar shadow-md' >
+            <div className='navbar-start'>
+                <Link href="/">
+                <a className="btn btn-ghost text-xl space-x-2">
+                    <IoMegaphoneSharp />
+                    <span>SCREAMER V2</span>
+                </a>
+                </Link>
+            </div>
+            <div className="navbar-end">
+                <Link href="/login">
+                    <a className="btn">LOGIN</a>
+                </Link>
+            </div>
         </div>
-    )
+    )   
 }
