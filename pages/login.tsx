9180b897941d06f00405adebaea249a1817f687b
AsianCrouton <keithlow1@hotmail.com>
2022-07-17 21:32:17 -0700

    login caps

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,21 +6,21 @@ export default function LoginPage() {
     <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
             <div className="text-center lg:text-left mb-4">
-                <h1 className="text-5xl font-bold">Login to Screamer</h1>
+                <h1 className="text-5xl font-bold">LOGIN TO SCREAMER</h1>
             </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
         <div className="card-body">
             <div className="form-control">
             <label className="label">
-                <span className="label-text">Email</span>
+                <span className="label-text">EMAIL</span>
             </label>
-            <input type="text" placeholder="email" className="input input-bordered" />
+            <input type="text" placeholder="EMAIL" className="input input-bordered" />
             </div>
             <div className="form-control">
             <label className="label">
-                <span className="label-text">Password</span>
+                <span className="label-text">PASSWORD</span>
             </label>
-            <input type="text" placeholder="password" className="input input-bordered" />
+            <input type="text" placeholder="PASSWORD" className="input input-bordered" />
             </div>
             <div className="form-control mt-6">
                 <button className="btn btn-primary">Login</button>
