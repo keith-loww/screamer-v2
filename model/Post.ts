04caff0b942ca9fe5c4d21d475685f79d87f5c68
AsianCrouton <keithlow1@hotmail.com>
2022-07-18 00:04:56 -0700

    normalize id

--- model/Post.ts
+++ model/Post.ts
@@ -16,4 +16,13 @@ const postSchema = new mongoose.Schema({
         }
     ]
 })
+
+postSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
 export default mongoose.models.Post || mongoose.model("Post", postSchema)
