77bc2fdb453f91244bdf2424c4b3bc56a0a79280
AsianCrouton <keithlow1@hotmail.com>
2022-07-18 20:49:15 -0700

    post display center

--- components/HomePage/PostsDisplay/index.tsx
+++ components/HomePage/PostsDisplay/index.tsx
@@ -4,7 +4,7 @@ import PostItem from './PostItem'
 
 export default function PostsDisplay({posts} : {posts: Post[]}): JSX.Element {
     return (
-        <div className='p-2'>
+        <div className='p-2 flex flex-col items-center'>
             {posts.map(post => (
                 <PostItem
                 key={post.id}
