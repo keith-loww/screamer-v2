8ce683f12196891866f8e4767fbb8afe50dfb5a3
AsianCrouton <keithlow1@hotmail.com>
2022-07-18 21:24:35 -0700

    apit post put

--- pages/api/posts/[id].ts
+++ pages/api/posts/[id].ts
@@ -16,6 +16,16 @@ export default async function handler(req: NextApiRequest, res : NextApiResponse
             } catch (error) {
                 return res.status(400).json({ success: false })
             }
+        case "PUT":
+            try {
+                const updated = await Post.findByIdAndUpdate(id, req.body)
+                return res.status(200).json({
+                    success: true,
+                    data: updated
+                })
+            } catch (error) {
+                return res.status(400).json({ success: false })
+            }
         default:
             return res.status(400).json({ success: false })
     }
