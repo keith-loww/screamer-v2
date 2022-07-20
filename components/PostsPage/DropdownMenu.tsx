1c24cf314a59eb1bef7c659466113b8de3dd9b46
AsianCrouton <keithlow1@hotmail.com>
2022-07-19 19:07:21 -0700

    drop down is now three dots

--- components/PostsPage/DropdownMenu.tsx
+++ components/PostsPage/DropdownMenu.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BsThreeDots } from 'react-icons/bs'
 
 interface PropTypes {
     deleteHandler: () => void
@@ -7,8 +8,10 @@ interface PropTypes {
 export default function DropdownMenu({deleteHandler} : PropTypes) {
     return (
         <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn m-1">Click</label>
-            <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+            <label tabIndex={0} className="btn btn-ghost rounded-full m-1">
+                <BsThreeDots />
+            </label>
+            <ul tabIndex={0} className="dropdown-content border menu p-2 shadow bg-base-100 rounded-box w-52">
                 <li><a onClick={deleteHandler}>Delete Post</a></li>
             </ul>
         </div>
