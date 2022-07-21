63e1d547aec62633a587a2f331d3b5d49b02dcd1
AsianCrouton <keithlow1@hotmail.com>
2022-07-21 15:23:14 -0700

    get  user with posts and authors api

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -43,3 +43,11 @@ export default async function handler(req: NextApiRequest, res : NextApiResponse
 
 export const getUser = async (id) => await User.findById(id)
 export const getUserWithPosts = async (id) => await User.findById(id).populate("posts")
+
+export const getUserWithPostsAndAuthors = async (id) => await User.findById(id).populate({
+    path: "posts",
+    populate: {
+        path: "author",
+        select: "nickname id picture"
+    }
+})
