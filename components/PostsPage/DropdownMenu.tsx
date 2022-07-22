4619067d2cca2e3b22312b5ef8f73006f02b6f4e
AsianCrouton <keithlow1@hotmail.com>
2022-07-22 01:54:00 -0700

    drop down icon

--- components/PostsPage/DropdownMenu.tsx
+++ components/PostsPage/DropdownMenu.tsx
@@ -1,6 +1,7 @@
 import { Menu } from '@mantine/core'
 import React from 'react'
 import { BsThreeDots } from 'react-icons/bs'
+import { FaRegTrashAlt } from 'react-icons/fa'
 
 interface PropTypes {
     deleteHandler: () => void
@@ -11,6 +12,7 @@ export default function DropdownMenu({deleteHandler} : PropTypes) {
         <Menu placement='end'>
             <Menu.Item
             color="red"
+            icon={<FaRegTrashAlt />}
             onClick={deleteHandler}>Delete</Menu.Item>
         </Menu>
     )
