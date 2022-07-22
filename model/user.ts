851f8fe6d316cdede19199a4be6a553ca227170c
AsianCrouton <keithlow1@hotmail.com>
2022-07-22 13:47:01 -0700

    weird fix

--- model/user.ts
+++ model/user.ts
@@ -30,7 +30,7 @@ const userSchema = new mongoose.Schema({
 userSchema.set("toJSON", {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id;
-        delete returnedObject._id
+        // delete returnedObject._id
         delete returnedObject.__v
     }
 })
