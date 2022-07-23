5635d9eba9da417f592bd914ae3b589a7c486575
AsianCrouton <keithlow1@hotmail.com>
2022-07-23 12:01:14 -0700

    drown down delets ocmments

--- components/CommentPage/CommentCard/DropDownMenu.tsx
+++ components/CommentPage/CommentCard/DropDownMenu.tsx
@@ -3,17 +3,38 @@ import { Comment } from '../../types'
 import { Menu } from '@mantine/core'
 import { useUser } from '@auth0/nextjs-auth0'
 import { FaRegTrashAlt } from 'react-icons/fa'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+import { showNotification } from '@mantine/notifications'
 
 interface PropTypes {
-    comment: Comment
+    comment: Comment,
 }
 
 const DropDownMenu = ({ comment }: PropTypes) => {
     const { user } = useUser()
+    const router = useRouter()
     if (!user) return null
 
-    const deleteHandler = () => {
-        console.log('delete comment')
+    const deleteHandler = async () => {
+        try {
+            await axios.delete(`/api/comments/${comment.id}`)
+            showNotification({
+                message: 'COMMENT SUCCESSFULLY DELETED',
+                color: "green",
+                icon: <FaRegTrashAlt />
+            })
+            if (comment.replyToType === 'Post') {
+                router.push(`/posts/${comment.replyTo.id}`)
+            } else {
+                router.push(`/comments/${comment.replyTo.id}`)
+            }
+        } catch (error) {
+            showNotification({
+                message: 'Error deleting comment',
+                color: "red"
+            })
+        }
     }
 
     return (
