201a155aa5e665127cdf4b16c743c15f1912a9b0
AsianCrouton <keithlow1@hotmail.com>
2022-07-23 01:52:39 -0700

    new comment form

--- components/CommentPage/CommentCard/NewCommentForm.tsx
+++ components/CommentPage/CommentCard/NewCommentForm.tsx
@@ -2,16 +2,49 @@ import { useUser } from '@auth0/nextjs-auth0'
 import { Avatar, Button, Textarea } from '@mantine/core'
 import React from 'react'
 import { Comment } from '../../types'
+import {useForm} from 'react-hook-form'
+import axios from 'axios'
+import { showNotification } from '@mantine/notifications'
+import { FaCheckCircle } from 'react-icons/fa'
+import { useRouter } from 'next/router'
 
 interface PropTypes {
     comment: Comment
 }
 
+interface FormData {
+    content: string
+}
+
 const NewCommentForm = ({ comment }: PropTypes) => {
     const { user } = useUser()
+    const router = useRouter()
+    const { register, handleSubmit, reset, formState : { errors }, setValue } = useForm<FormData>()
+
     if (!user) return null
+
+    const submitHandler = async (data: any) => {
+        const { content } = data
+        const obj = {
+            content,
+            author: user.sub,
+            replyToType: "Comment",
+            replyTo: comment.id
+        }
+        await axios.post("/api/comments", obj)
+        showNotification({
+            message: "SUCCESSFULLY SCREAMED BACK",
+            color: "green",
+            icon: <FaCheckCircle />
+        })
+        reset()
+        router.replace(router.asPath)
+    }
+
     return (
-        <div className='w-full flex flex-row flex-wrap space-y-2'>
+        <form
+        onSubmit={handleSubmit(submitHandler)}
+        className='w-full flex flex-row flex-wrap space-y-2'>
             <div className='flex flex-row space-x-2 w-full items-center'>
                 <Avatar
                 className='rounded-full'
@@ -19,12 +52,25 @@ const NewCommentForm = ({ comment }: PropTypes) => {
                 size="md"
                 />
                 <Textarea
-                placeholder='SCREAM BACK'
+                {...register("content", {
+                    maxLength : {
+                        value: 280,
+                        message: 'COMMENT CANNOT EXCEED 280 CHARACTERS'
+                    },
+                    minLength : {
+                        value: 5,
+                        message: 'COMMENT MUST BE AT LEAST 5 CHARACTERS LONG'
+                    }
+                })}
+                error={errors ? errors.content?.message : null}
+                onChange={(e) => setValue("content", e.target.value.toUpperCase())}
+                placeholder='SCREAM BACK...'
                 className='w-full'
                 autosize />
             </div>
             <div className='w-full flex flex-row justify-end'>
                 <Button
+                type='submit'
                 className='w-1/4'
                 uppercase
                 variant='outline'
@@ -32,7 +78,7 @@ const NewCommentForm = ({ comment }: PropTypes) => {
                     REPLY
                 </Button>
             </div>
-        </div>
+        </form>
     )
 }
 
