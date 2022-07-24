739ee7ec94983b60cb62edc1602863776e2ca6cc
AsianCrouton <keithlow1@hotmail.com>
2022-07-23 19:08:27 -0700

    local storage for light dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,21 @@ import type { AppProps } from 'next/app'
 import { UserProvider } from '@auth0/nextjs-auth0';
 import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
-import { useState } from 'react';
+import { useLocalStorage } from '@mantine/hooks';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [colorScheme, setColorScheme] = useState<ColorScheme>('dark')
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'mantine-color-scheme',
+    defaultValue: 'light',
+    getInitialValueInEffect: true,
+  });
   const toggleColorScheme = (value?: ColorScheme) => setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
 
   return (
     <UserProvider>
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme} >
-        <MantineProvider theme={{ colorScheme }} withGlobalStyles withCSSVariables >
+        <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS >
           <NotificationsProvider>
             <Component {...pageProps} />
           </NotificationsProvider>
