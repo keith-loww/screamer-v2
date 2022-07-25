7ed318bb853c410f16570ed8f5be1f1244c9822b
AsianCrouton <keithlow1@hotmail.com>
2022-07-25 01:19:54 -0700

    comment card liking notif

--- components/CommentPage/CommentCard/LikeDisplay.tsx
+++ components/CommentPage/CommentCard/LikeDisplay.tsx
@@ -1,9 +1,11 @@
 import { useUser } from '@auth0/nextjs-auth0'
 import { ActionIcon } from '@mantine/core'
+import { showNotification, updateNotification } from '@mantine/notifications'
 import axios from 'axios'
 import { useRouter } from 'next/router'
-import React from 'react'
-import { AiFillLike, AiOutlineLike } from 'react-icons/ai'
+import React, { useState } from 'react'
+import { AiFillDislike, AiFillLike, AiOutlineExclamation, AiOutlineLike } from 'react-icons/ai'
+import { getRandomId } from '../../../lib/LikePostNotifId'
 import { Comment, CommentData } from '../../types'
 
 interface PropTypes {
@@ -13,26 +15,58 @@ interface PropTypes {
 const LikeDisplay = ({ comment }: PropTypes) => {
     const { user } = useUser()
     const router = useRouter()
+    const [likeLoading, setLikeLoading] = useState(false)
     const alreadyLiked = (user && user.sub && comment.likedBy.includes(user.sub))
 
     const likeHandler = async () => {
-        if (!user) router.push("/api/auth/login")
-        const resp = await axios.get(`/api/comments/${comment.id}`)
-        const originalComment : CommentData = resp.data.data
-        let newObj
-        if (alreadyLiked) {
-            newObj = {
-                ...originalComment,
-                likedBy: comment.likedBy.filter(id => id !== user.sub)
-            }   
-        } else {
-            newObj = {
-                ...originalComment,
-                likedBy: [...comment.likedBy, user?.sub]
+        const notifID = getRandomId()
+        try {
+            if (!user) router.push("/api/auth/login")
+            setLikeLoading(true)
+            showNotification({
+                id: `like-comment-${comment.id}-${notifID}`,
+                message: alreadyLiked ? "Unliking..." : "Liking...",
+                loading: true,
+                autoClose: false,
+                disallowClose: true
+            })
+            const resp = await axios.get(`/api/comments/${comment.id}`)
+            const originalComment : CommentData = resp.data.data
+            let newObj
+            if (alreadyLiked) {
+                newObj = {
+                    ...originalComment,
+                    likedBy: comment.likedBy.filter(id => id !== user.sub)
+                }   
+            } else {
+                newObj = {
+                    ...originalComment,
+                    likedBy: [...comment.likedBy, user?.sub]
+                }
             }
+            await axios.put(`/api/comments/${comment.id}`, newObj)
+            updateNotification({
+                id: `like-comment-${comment.id}-${notifID}`,
+                message: alreadyLiked ? "SUCESSFULLY UNLIKED COMMENT" : "SUCCESSFULLY LIKED COMMENT",
+                loading: false,
+                autoClose: true,
+                color: "green",
+                icon: alreadyLiked ? <AiFillDislike /> : <AiFillLike />
+            })
+            setLikeLoading(false)
+            router.replace(router.asPath)
+        } catch (error) {
+            console.log(error)
+            setLikeLoading(false)
+            updateNotification({
+                message: "ERROR LIKING COMMENT",
+                color: "red",
+                icon: <AiOutlineExclamation />,
+                autoClose: true,
+                id: `like-comment-${comment.id}-${notifID}`,
+                loading: false
+            })
         }
-        await axios.put(`/api/comments/${comment.id}`, newObj)
-        router.replace(router.asPath)
     }
 
     return (
@@ -41,7 +75,8 @@ const LikeDisplay = ({ comment }: PropTypes) => {
                 <ActionIcon
                 size="lg"
                 variant='transparent'
-                onClick={likeHandler} >
+                onClick={likeHandler}
+                disabled={likeLoading} >
                     {alreadyLiked
                     ? <AiFillLike size={20} />
                     : <AiOutlineLike size={20} />}
