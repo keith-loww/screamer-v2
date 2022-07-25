55d48a51b71a01295ac650037648c3e88da172d2
AsianCrouton <keithlow1@hotmail.com>
2022-07-25 01:24:02 -0700

    reply to like notif

--- components/CommentPage/ReplyToCard/LikeAndCommentDisplay.tsx
+++ components/CommentPage/ReplyToCard/LikeAndCommentDisplay.tsx
@@ -1,11 +1,13 @@
 import { useUser } from '@auth0/nextjs-auth0'
 import { ActionIcon } from '@mantine/core'
+import { showNotification, updateNotification } from '@mantine/notifications'
 import axios from 'axios'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React from 'react'
-import { AiFillLike, AiOutlineLike } from 'react-icons/ai'
+import React, { useState } from 'react'
+import { AiFillDislike, AiFillLike, AiOutlineExclamation, AiOutlineLike } from 'react-icons/ai'
 import { BiCommentError } from 'react-icons/bi'
+import { getRandomId } from '../../../lib/LikePostNotifId'
 import { ReplyTo, ReplyToType } from '../../types'
 
 interface PropTypes {
@@ -20,30 +22,62 @@ const isPost = (type: ReplyToType): boolean => {
 const LikeAndCommentDisplay = ({ replyTo, type }: PropTypes) => {
     const {user} = useUser()
     const router = useRouter()
+    const [likeLoading, setLikeLoading] = useState(false)
     const alreadyLiked = (user && user.sub && replyTo.likedBy.includes(user?.sub));
 
     const likeHandler = async () => {
         if (!user) router.push("/api/auth/login")
-        let updatedReplyTo
-        if (alreadyLiked) {
-            updatedReplyTo = {
-                ...replyTo,
-                author: replyTo.author.id,
-                likedBy: replyTo.likedBy.filter(id => id !== user?.sub)
+        const notifID = getRandomId()
+        try {
+            setLikeLoading(true)
+            showNotification({
+                id: `like-${type}-${replyTo.id}-${notifID}`,
+                message: alreadyLiked ? "Unliking..." : "Liking...",
+                loading: true,
+                autoClose: false,
+                disallowClose: true
+            })
+            let updatedReplyTo
+            if (alreadyLiked) {
+                updatedReplyTo = {
+                    ...replyTo,
+                    author: replyTo.author.id,
+                    likedBy: replyTo.likedBy.filter(id => id !== user?.sub)
+                }
+            } else {
+                updatedReplyTo = {
+                    ...replyTo,
+                    author: replyTo.author.id,
+                    likedBy: [...replyTo.likedBy, user?.sub]
+                }
             }
-        } else {
-            updatedReplyTo = {
-                ...replyTo,
-                author: replyTo.author.id,
-                likedBy: [...replyTo.likedBy, user?.sub]
+            if (isPost(type)) {
+                await axios.put(`/api/posts/${replyTo.id}`, updatedReplyTo)
+            } else {
+                await axios.put(`/api/comments/${replyTo.id}`, updatedReplyTo)
             }
+            setLikeLoading(false)
+            updateNotification({
+                id: `like-${type}-${replyTo.id}-${notifID}`,
+                message: alreadyLiked ? `SUCCESSFULLY UNLIKED ${type.toUpperCase()}` : `SUCCESSFULLY LIKED ${type.toUpperCase()}`,
+                loading: false,
+                autoClose: true,
+                color: "green",
+                icon: alreadyLiked ? <AiFillDislike /> : <AiFillLike />
+            })
+            router.replace(router.asPath)
+        } catch (error) {
+            console.log(error)
+            setLikeLoading(false)
+            updateNotification({
+                message: "ERROR LIKING",
+                color: "red",
+                icon: <AiOutlineExclamation />,
+                autoClose: true,
+                id: `like-${type}-${replyTo.id}-${notifID}`,
+                loading: false
+            })
         }
-        if (isPost(type)) {
-            await axios.put(`/api/posts/${replyTo.id}`, updatedReplyTo)
-        } else {
-            await axios.put(`/api/comments/${replyTo.id}`, updatedReplyTo)
-        }
-        router.replace(router.asPath)
     }
     return (
         <div className='flex justify-start space-x-6'>
@@ -51,6 +85,7 @@ const LikeAndCommentDisplay = ({ replyTo, type }: PropTypes) => {
                     <ActionIcon
                     variant='transparent'
                     onClick={likeHandler}
+                    disabled={likeLoading}
                     >
                         {alreadyLiked ? <AiFillLike /> : <AiOutlineLike />}
                     </ActionIcon>
