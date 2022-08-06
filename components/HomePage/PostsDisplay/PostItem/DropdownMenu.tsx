dfc8ec310dc052ac4d7c80a2c4eebf3f29c78b6d
AsianCrouton <keithlow1@hotmail.com>
2022-08-06 15:20:11 -0700

    post item dropdown menu

--- components/HomePage/PostsDisplay/PostItem/DropdownMenu.tsx
+++ components/HomePage/PostsDisplay/PostItem/DropdownMenu.tsx
@@ -1,5 +1,6 @@
-import { Menu } from '@mantine/core'
+import { ActionIcon, Menu } from '@mantine/core'
 import React from 'react'
+import { BsThreeDots } from 'react-icons/bs'
 import { FaRegTrashAlt } from 'react-icons/fa'
 
 interface PropTypes {
@@ -8,11 +9,24 @@ interface PropTypes {
 
 export default function DropdownMenu({deleteHandler} : PropTypes) {
     return (
-        <Menu placement='end'>
-            <Menu.Item
-            color="red"
-            icon={<FaRegTrashAlt />}
-            onClick={deleteHandler}>Delete</Menu.Item>
+        <Menu
+        width={200}
+        withinPortal
+        shadow="md"
+        position='bottom-end'
+        transition="scale" >
+            <Menu.Target>
+                <ActionIcon variant='subtle'>
+                    <BsThreeDots />
+                </ActionIcon>
+            </Menu.Target>
+            
+            <Menu.Dropdown>
+                <Menu.Item
+                color="red"
+                icon={<FaRegTrashAlt />}
+                onClick={deleteHandler}>Delete</Menu.Item>
+            </Menu.Dropdown>
         </Menu>
     )
 }
